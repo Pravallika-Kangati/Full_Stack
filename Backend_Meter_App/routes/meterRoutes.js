3c5604a0a5f20a0509074ffbcaa7c90907a7c8c1
Pravallika-Kangati <63946766+Pravallika-Kangati@users.noreply.github.com>
2020-06-19 08:49:16 +0530

    Update meterRoutes.js

--- Backend_Meter_App/routes/meterRoutes.js
+++ Backend_Meter_App/routes/meterRoutes.js
@@ -1,34 +1,16 @@
 const meterRouter = (app, fs) => {
-// variables
-const dataPath = "./data/metering_data.json";
+	// variables
+	const dataPath = './data/metering_data.json';
 
-// READ
-app.get("/meter_data/:Serial", (req, res) => {
-
-    // Reading isbn from the URL
-    const serial = req.params.Serial;
-    fs.readFile(dataPath, "utf8", (err, data) => {
-        if(err){
-            throw err;
-        }
-        // json data
-        var jsonData = data;
-
-        //create empty json object
-        var searchData = [];
-
-        // parse json
-        var jsonParsed = JSON.parse(jsonData);
-
-        const meterReading = jsonParsed.map((meter) => {
-            if(meter.Serial === serial){
-                searchData.push(meter);
-            }
-            return searchData;
-        });
-        return res.status(200).json(searchData);
-    });
-    });
+	// READ
+	app.get('/meter_data', (req, res) => {
+		fs.readFile(dataPath, 'utf8', (err, data) => {
+			if (err) {
+				throw err;
+			}
+			return res.status(200).send(JSON.parse(data));
+		});
+	});
 };
 
 module.exports = meterRouter;
